062bd668257fcb0776d770c34318c6bce015297f
Bobba <sarathchandra.bobba@gmail.com>
2020-10-07 19:05:14 +0530

    Update bobba.py

--- bobba.py
+++ bobba.py
@@ -1,6 +1,6 @@
 from github import Github
 
-g = Github('sarathchandra.bobba@gmail.com', 'iton@123')
+g = Github('sarathchandra.bobba@gmail.com', '')
 
 for repo in g.get_user().get_repos():
 	print repo.name
